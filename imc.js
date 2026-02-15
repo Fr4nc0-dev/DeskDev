61f17ca35ba3e6d43e5d160797597cd1b11c5600
Rodrigo Franco <rodrigo.franco9@gmail.com>
2026-02-15 15:11:22 -0300

    Alteração do HTML e JS.
    Foram feitas as correções do JS e ajustes no html.

--- imc.js
+++ imc.js
@@ -1,37 +1,9 @@
-console.log("Helo, Baby!");
-
-//let pesoPrompt = prompt("Digite seu peso: ");
-//let alturaPrompt = prompt("Digite sua altura: ");
-
-//console.log(typeof pesoPrompt);
-//console.log(typeof alturaPrompt);
-
-const pesoInput = document.getElementById("peso");
-const alturaInput = document.getElementById("altura");
-
-function calcularImc() {
-    let calculo = pesoInput / alturaInput ** 2;
-
-    return calculo;
-}
-
-console.log(calcularImc(78, 1.84));
-
-function retornarCategoria() {
-    const resultadoImc = calcularImc(pesoInput.value, alturaInput.value);
-    if (resultadoImc < 18.5) {
-        return "Abaixo do peso";
-    } else if (resultadoImc >= 18.5 && resultadoImc <= 24.9) {
-        return "Peso normal";
-    } else if (resultadoImc >=25 && resultadoImc <= 29.9) {
-        return "Sobrepeso";
-    } else {
-        return "Obesidade";
+    function calcularImc() {
+    const peso1 = window.document.getElementById("peso");
+    const altura2 = window.document.getElementById("altura");
+    const n1 = Number(peso1.value);
+    const n2 = Number(altura2.value);
+    const Imc = n1 / n2 ** 2;
+    const res = window.document.getElementById("res");
+    res.innerHTML = `O resultado do seu IMC é<strong>${Imc}</strong>!!!.`;
     }
-}
-
-function exibirResultado() {
-    console.log(calcularImc(pesoInput.value, alturaInput.value), retornarCategoria());
-}
-
-console.log(retornarCategoria());
