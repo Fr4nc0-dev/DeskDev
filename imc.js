742bf7ba44a23a3ac226554e922fe7997ba8aca1
Rodrigo Franco <rodrigo.franco9@gmail.com>
2026-02-22 15:47:49 -0300

    Correção da semântica
    Foi feita a correção e retirada dos parênteses interno da função reportar().

--- imc.js
+++ imc.js
@@ -11,15 +11,18 @@
     function reportar() {
         if (res <= 18,5) {
             console.log('Você está abaixo do peso.');
-        } else if (res > 18.5) || (res < 24.9) {
+        } else if (res > 18.5 || res < 24.9) {
             console.log('Você está com o peso normal.');
-        } else if (res > 25) || (res < 29.9) {
+        } else if (res > 25 || res < 29.9) {
             console.log('Você está com sobrepeso.');
-        } else if (res > 30) || (res < 34.9) {
+        } else if (res > 30 || res < 34.9) {
             console.log('Você está com obesidade grau I.');
-        } else if (res > 35) || (res < 39.9) {
+        } else if (res > 35 || res < 39.9) {
             console.log('Você está com obesidade grau II.');
         } else {
             console.log('Você está com obesidade grau III! Procure um médico.')
         }
+        
     }
+
+reportar();
