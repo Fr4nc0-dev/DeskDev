7fd131942e3fd29c07dc10c6343656984464b9b8
Rodrigo Franco <rodrigo.franco9@gmail.com>
2026-02-21 22:27:31 -0300

    IMC1.2
    Desenvolvendo a função da classificação do IMC. Em análise...

--- imc.js
+++ imc.js
@@ -7,3 +7,19 @@
     const res = window.document.getElementById("res");
     res.innerHTML = `O resultado do seu IMC é <strong>${Imc}</strong>!!!.`;
     }
+
+    function reportar() {
+        if (res <= 18,5) {
+            console.log('Você está abaixo do peso.');
+        } else if (res > 18.5) || (res < 24.9) {
+            console.log('Você está com o peso normal.');
+        } else if (res > 25) || (res < 29.9) {
+            console.log('Você está com sobrepeso.');
+        } else if (res > 30) || (res < 34.9) {
+            console.log('Você está com obesidade grau I.');
+        } else if (res > 35) || (res < 39.9) {
+            console.log('Você está com obesidade grau II.');
+        } else {
+            console.log('Você está com obesidade grau III! Procure um médico.')
+        }
+    }
